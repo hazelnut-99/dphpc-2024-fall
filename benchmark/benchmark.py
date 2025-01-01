b643871e183370a8358b4296e984544143edf502
haise <haise@pinduoduo.com>
2025-01-01 11:00:36 +0100

    fix

--- benchmark/benchmark.py
+++ benchmark/benchmark.py
@@ -8,35 +8,34 @@ import uuid
 
 
 train_configs = [
-    {'gpus': 1, 'dp': 1, 'sp_ring': 1, 'sp_ulysses': 1, 'num_attention_heads': 4},
+    {'gpus_avail': 1, 'per_node_gpus': 1, 'node_cnt': 1, 'dp': 1, 'sp_ring': 1, 'sp_ulysses': 1, 'num_attention_heads': 4},
 
-    {'gpus': 2, 'dp': 2, 'sp_ring': 1, 'sp_ulysses': 1, 'num_attention_heads': 4},
-    {'gpus': 2, 'dp': 1, 'sp_ring': 2, 'sp_ulysses': 1, 'num_attention_heads': 4},
-    {'gpus': 2, 'dp': 1, 'sp_ring': 1, 'sp_ulysses': 2, 'num_attention_heads': 4},
+    {'gpus_avail': 2, 'per_node_gpus': 2, 'node_cnt': 1, 'dp': 2, 'sp_ring': 1, 'sp_ulysses': 1, 'num_attention_heads': 4},
+    {'gpus_avail': 2, 'per_node_gpus': 2, 'node_cnt': 1, 'dp': 1, 'sp_ring': 2, 'sp_ulysses': 1, 'num_attention_heads': 4},
+    {'gpus_avail': 2, 'per_node_gpus': 2, 'node_cnt': 1, 'dp': 1, 'sp_ring': 1, 'sp_ulysses': 2, 'num_attention_heads': 4},
 
-    {'gpus': 4, 'dp': 4, 'sp_ring': 1, 'sp_ulysses': 1, 'num_attention_heads': 4},
-    {'gpus': 4, 'dp': 1, 'sp_ring': 4, 'sp_ulysses': 1, 'num_attention_heads': 4},
-    {'gpus': 4, 'dp': 1, 'sp_ring': 1, 'sp_ulysses': 4, 'num_attention_heads': 4},
-    {'gpus': 4, 'dp': 1, 'sp_ring': 2, 'sp_ulysses': 2, 'num_attention_heads': 4},
+    {'gpus_avail': 4, 'per_node_gpus': 4, 'node_cnt': 1, 'dp': 4, 'sp_ring': 1, 'sp_ulysses': 1, 'num_attention_heads': 4},
+    {'gpus_avail': 4, 'per_node_gpus': 4, 'node_cnt': 1, 'dp': 1, 'sp_ring': 4, 'sp_ulysses': 1, 'num_attention_heads': 4},
+    {'gpus_avail': 4, 'per_node_gpus': 4, 'node_cnt': 1, 'dp': 1, 'sp_ring': 1, 'sp_ulysses': 4, 'num_attention_heads': 4},
+    {'gpus_avail': 4, 'per_node_gpus': 4, 'node_cnt': 1, 'dp': 1, 'sp_ring': 2, 'sp_ulysses': 2, 'num_attention_heads': 4},
 
-    {'gpus': 4, 'dp': 2, 'sp_ring': 2, 'sp_ulysses': 1, 'num_attention_heads': 4},
-    {'gpus': 4, 'dp': 2, 'sp_ring': 1, 'sp_ulysses': 2, 'num_attention_heads': 4},
+    {'gpus_avail': 4, 'per_node_gpus': 4, 'node_cnt': 1, 'dp': 2, 'sp_ring': 2, 'sp_ulysses': 1, 'num_attention_heads': 4},
+    {'gpus_avail': 4, 'per_node_gpus': 4, 'node_cnt': 1, 'dp': 2, 'sp_ring': 1, 'sp_ulysses': 2, 'num_attention_heads': 4},
 
 
+    {'gpus_avail': 1, 'per_node_gpus': 4, 'node_cnt': 1, 'dp': 1, 'sp_ring': 1, 'sp_ulysses': 1, 'num_attention_heads': 2},
 
-    {'gpus': 1, 'dp': 1, 'sp_ring': 1, 'sp_ulysses': 1, 'num_attention_heads': 2},
+    {'gpus_avail': 2, 'per_node_gpus': 2, 'node_cnt': 1, 'dp': 2, 'sp_ring': 1, 'sp_ulysses': 1, 'num_attention_heads': 2},
+    {'gpus_avail': 2, 'per_node_gpus': 2, 'node_cnt': 1, 'dp': 1, 'sp_ring': 2, 'sp_ulysses': 1, 'num_attention_heads': 2},
+    {'gpus_avail': 2, 'per_node_gpus': 2, 'node_cnt': 1, 'dp': 1, 'sp_ring': 1, 'sp_ulysses': 2, 'num_attention_heads': 2},
 
-    {'gpus': 2, 'dp': 2, 'sp_ring': 1, 'sp_ulysses': 1, 'num_attention_heads': 2},
-    {'gpus': 2, 'dp': 1, 'sp_ring': 2, 'sp_ulysses': 1, 'num_attention_heads': 2},
-    {'gpus': 2, 'dp': 1, 'sp_ring': 1, 'sp_ulysses': 2, 'num_attention_heads': 2},
+    {'gpus_avail': 4, 'per_node_gpus': 4, 'node_cnt': 1, 'dp': 4, 'sp_ring': 1, 'sp_ulysses': 1, 'num_attention_heads': 2},
+    {'gpus_avail': 4, 'per_node_gpus': 4, 'node_cnt': 1, 'dp': 1, 'sp_ring': 4, 'sp_ulysses': 1, 'num_attention_heads': 2},
+    {'gpus_avail': 4, 'per_node_gpus': 2, 'node_cnt': 1, 'dp': 1, 'sp_ring': 1, 'sp_ulysses': 2, 'num_attention_heads': 2},
+    {'gpus_avail': 4, 'per_node_gpus': 4, 'node_cnt': 1, 'dp': 1, 'sp_ring': 2, 'sp_ulysses': 2, 'num_attention_heads': 2},
 
-    {'gpus': 4, 'dp': 4, 'sp_ring': 1, 'sp_ulysses': 1, 'num_attention_heads': 2},
-    {'gpus': 4, 'dp': 1, 'sp_ring': 4, 'sp_ulysses': 1, 'num_attention_heads': 2},
-    {'gpus': 4, 'dp': 1, 'sp_ring': 1, 'sp_ulysses': 2, 'num_attention_heads': 2},
-    {'gpus': 4, 'dp': 1, 'sp_ring': 2, 'sp_ulysses': 2, 'num_attention_heads': 2},
-
-    {'gpus': 4, 'dp': 2, 'sp_ring': 2, 'sp_ulysses': 1, 'num_attention_heads': 2},
-    {'gpus': 4, 'dp': 2, 'sp_ring': 1, 'sp_ulysses': 2, 'num_attention_heads': 2},
+    {'gpus_avail': 4, 'per_node_gpus': 4, 'node_cnt': 1, 'dp': 2, 'sp_ring': 2, 'sp_ulysses': 1, 'num_attention_heads': 2},
+    {'gpus_avail': 4, 'per_node_gpus': 4, 'node_cnt': 1, 'dp': 2, 'sp_ring': 1, 'sp_ulysses': 2, 'num_attention_heads': 2},
 
 
 ]
@@ -49,7 +48,10 @@ def render_yaml_content(conf):
     data['parallelism']['dp'] = conf['dp']
     data['parallelism']['sp_ring'] = conf['sp_ring']
     data['parallelism']['sp_ulysses'] = conf['sp_ulysses']
+
     data['model']['model_config']['num_attention_heads'] = conf['num_attention_heads']
+    data['model']['model_config']['num_key_value_heads'] = conf['num_attention_heads']
+
     data['tokens']['sequence_length'] = conf['sequence_length']
     data['model']['model_config']['max_position_embeddings'] = conf['sequence_length']
 
@@ -80,8 +82,8 @@ def prepare_one_config(conf, config_path):
     with open(f'{top_directory}/conf.yaml', 'w') as file:
         yaml.dump(render_yaml_content(conf), file)
 
-    with open(f'{top_directory}/gpus', 'w') as file:
-        file.write(str(conf['gpus']))
+    with open(f'{top_directory}/per_node_gpus', 'w') as file:
+        file.write(str(conf['per_node_gpus']))
 
     with open(f'{top_directory}/parameters.json', 'w') as json_file:
         json.dump(conf, json_file, indent=4)
@@ -92,7 +94,7 @@ def prepare_configs():
     print(f"working directory prefix: {prefix}")
     for conf in train_configs:
         seq_len = 256
-        while seq_len <= (65536 * 16):
+        while seq_len <= (65536 * 2):
             conf['sequence_length'] = seq_len
             generated_uuid = str(uuid.uuid4())
             config_path = f"{prefix}/{generated_uuid}"
